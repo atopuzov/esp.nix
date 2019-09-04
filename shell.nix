1e405c5603a154199f888c44ffb564a7a1f7e754
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2019-09-04 23:05:34 +0100

    esp8266 arduino works with esp-quick-toolchain
    * TODO fix makeEspArduino `runtime.tools.python.path`

--- shell.nix
+++ shell.nix
@@ -12,6 +12,13 @@ let
       sha256 = "0ylsh9xx3cypybr1066p7d93i1ki0vvncb5vhcdvcjb35vl6lj08";
     };
   });
+  xtensa-lx106-arduino = xtensa-lx106.overrideAttrs (oldAttrs: rec {
+    name = "xtensa-lx106-2.5.0-4";
+    src = pkgs.fetchurl {
+      url = "https://github.com/earlephilhower/esp-quick-toolchain/releases/download/2.5.0-4/x86_64-linux-gnu.xtensa-lx106-elf-b40a506.1563313032.tar.gz";
+      sha256 = "1000f022rxzqb09h21q5cjkh8qn4dbsf4cfjjxhg7272kfj4vq4h";
+    };
+  });
   makeEspArduino = pkgs.callPackage ./makeEspArduino.nix {};
 in
   pkgs.stdenv.mkDerivation {
@@ -25,7 +32,7 @@ in
       export LANG=en_US.UTF-8
       export IDF_PATH=${esp8266_rtos_sdk}
       export ESP_ROOT=${esp8266_arduino}
-      export COMP_PATH=${xtensa-lx106-old}
+      export COMP_PATH=${xtensa-lx106-arduino}
       alias makeesparduino="make -f ${makeEspArduino}/makeEspArduino.mk"
       echo "Welcome!"
     '';
