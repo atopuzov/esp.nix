b80fac46513e47786967b4854c2e4470f7ac00cf
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2019-09-04 17:44:47 +0100

    Precompile *conf-idf tool

--- esp8266-rtos-sdk.nix
+++ esp8266-rtos-sdk.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl }:
+{ stdenv, fetchurl, ncurses, flex, bison, gperf, pkg-config }:
 
 stdenv.mkDerivation rec {
   name = "esp8266-rtos-sdk-${version}";
@@ -9,12 +9,21 @@ stdenv.mkDerivation rec {
     sha256 = "0zx1p7kar1mpnkxy6m7w42v5pdmfvy17g3k4xscrj0i1gma8piaz";
   };
 
+  nativeBuildInputs = [ ncurses flex bison gperf pkg-config ];
+
   # src = fetchFromGitHub {
   #   owner = "espressif";
   #   repo = "ESP8266_RTOS_SDK";
   #   rev = "";
   #   sha256 = "";
   # };
+
+  buildPhase = ''
+    # Compile conf-idf and mconf-idf
+    cd tools/kconfig
+    make
+    cd ../..
+  '';
 
   installPhase = ''
     mkdir -p $out
