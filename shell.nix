92051040c62bf2cb35b9e2c668828c9e169ecc98
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2019-09-04 18:35:40 +0100

    Nix shell

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ nixpkgs ? <nixpkgs>
+}:
+let
+  pkgs = import nixpkgs {};
+  esp8266_rtos_sdk = pkgs.callPackage ./esp8266-rtos-sdk.nix  {};
+  xtensa-lx106 =  pkgs.callPackage ./xtensa-lx106.nix {};
+in
+  pkgs.stdenv.mkDerivation {
+    name = "esp8266-shell";
+    buildInputs = [ esp8266_rtos_sdk xtensa-lx106 ];
+    shellHook = ''
+      export LANG=en_US.UTF-8
+      export IDF_PATH=${esp8266_rtos_sdk}
+      echo "Welcome!"
+    '';
+  }
